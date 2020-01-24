91a1d6ea3dbec13e4c3d7e65354672329e649d96
Hawxy <Hawxy@users.noreply.github.com>
2020-01-24 15:57:40 +0800

    Add extension

---
+++ source/Nuke.Common/Tools/Pulumi/PulumiTasks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Nuke.Common.Tooling;
+using Nuke.Common.Utilities;
+
+namespace Nuke.Common.Tools.Pulumi
+{
+    public partial class PulumiTasks
+    {
+        public static T GetStackOutput<T>(PulumiStackOutputSettings toolSettings = null)
+        {
+            toolSettings ??= new PulumiStackOutputSettings();
+            var process = ProcessTasks.StartProcess(toolSettings.EnableJson()); 
+            process.AssertZeroExitCode();
+            var output = process.Output.EnsureOnlyStd().Select(x => x.Text).ToList();
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(string.Join("\r\n", output));
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(
+                    new[] { $"{nameof(Pulumi)} exited with code {process.ExitCode}, but cannot parse output as JSON:" }
+                        .Concat(process.Output.Select(x => x.Text)).JoinNewLine(),
+                    exception);
+            }
+        }
+    }
+}
